7c1c6351617b84fe146e744075fb84b5ccfdb958
nexuslee <tonexus.lee@gmail.com>
2014-11-08 16:51:09 +0800

    connect-mongostore模块使用 session

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var http = require('http');
-var ejs =require('ejs');
+var ejs = require('ejs');
 
+//var mongoose = require('mongoose');
+//mongoose.connect("mongodb://localhost/mysessionstore");
+
+var session = require('express-session');
+var MongoStore = require("connect-mongostore")(session);
+//var store = new SessionStore({
+//    connection: mongoose.connection,
+//    interval: 120000
+//});
 var app = express();
 
 // view engine setup
@@ -23,6 +32,19 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+//app.use(express.cookieSession({secret:'nexus'}));
+app.use(session({
+    secret: 'nexus',
+    store: new MongoStore({'db': 'node-demo'}),
+    cookie: {maxAge: 900000},
+    saveUninitialized: true,
+    resave: true
+}));
+
+//app.use(function (req, res, next) {
+//    res.locals.user = req.session.user;
+//})
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -30,12 +52,12 @@ app.use('/users', users);
 
 //app.use('/login', routes);
 /*
-app.post('/login', routes);
-app.get('/logout', routes);
-app.get('/home', routes);*/
+ app.post('/login', routes);
+ app.get('/logout', routes);
+ app.get('/home', routes);*/
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -46,7 +68,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -57,7 +79,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
