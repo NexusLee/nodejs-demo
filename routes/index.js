3827c74d75a7aea3260e255694515e97bcd6cddf
NexusLee <tonexus.lee@gmail.com>
2015-01-11 00:02:16 +0800

    修整理代码

--- routes/index.js
+++ routes/index.js
@@ -17,34 +17,13 @@ router.get('/login', function(req, res){
     res.render('login', { title: '用户登陆'});
 });
 
-router.post('/login',notAuthentication);
+router.post('/login',checkLogin);
 router.post('/login',doLogin);
 
 router.get('/home',authentication);
 router.get('/home', function(req, res){
     res.render('home', { title: '用户登陆'});
 });
-//router.get('/mypage',authentication);
-//router.get('/home',function(req, res){
- //       authentication();
-
- // });
-
-/*
-router.doLogin = function(req, res){
-    var user={
-        username:'admin',
-        password:'admin'
-    }
-    if(req.body.username===user.username && req.body.password===user.password){
-        req.session.user=user;
-        res.redirect('/home');
-    }
-    else {
-        req.session.error = '用户名或密码不正确';
-        res.redirect('/login');
-    }
-}; */
 
 function checkNotLogin(req, res, next) {
     if (req.session.user) {
@@ -62,7 +41,7 @@ function authentication(req, res, next) {
      }
     next();
 }
-function notAuthentication (req, res, next) {
+function checkLogin (req, res, next) {
     if (req.session.user) {
         req.session.error='已登陆';
         return res.redirect('/');
@@ -76,21 +55,12 @@ function doLogin(req, res){
     }
     if(req.body.username===user.username && req.body.password===user.password){
         req.session.user=user;
-       // res.render('mypage', { title: '我的页面'});
-      //  if(err) console.log(err);
-        //res.redirect('/mypage');
-       // res.render('home', { title: '我的页面'});
         res.redirect('/home');
-    //return res.redirect('/login');
-       // res.redirect('/home');
-      //  if(err) console.log(err);
-        //res.redirect('/home',{ title: '用户登陆'});
 
     }
     else {
         req.session.error = '用户名或密码不正确';
         res.redirect('/login');
-       //res.redirect('/login',{title: '用户登陆'});
     }
 }
 
